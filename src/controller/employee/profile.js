9c5bde5bcd02b12fcf7144c06e229ba977daa982
Joyet1021 <159019597+Joyet1021@users.noreply.github.com>
2024-07-05 19:20:21 +0530

    Implemented change password functionality

--- src/controller/employee/profile.js
+++ src/controller/employee/profile.js
@@ -49,3 +49,45 @@ exports.profilePut = async (req, res) => {
         res.status(500).json({ message: 'There was an error updating the profile' });
     }
 };
+exports.updatePasswordPost = async (req, res) => {
+    try {
+      const id = req.query.id;
+      const { oldPassword, newPassword } = req.body;
+  
+      // Validate the new password with regex
+      const validatePassword = passwordRegex.test(newPassword);
+  
+      // Fetch the employee by id
+      const employee = await signupModel.findOne({ _id: id });
+      if (!employee) {
+        return res.status(404).json({ message: 'Employee not found!' });
+      }
+  
+      // Compare old password with the employee's current password
+      const passmatch = await bcryptjs.compare(oldPassword, employee.password);
+      if (!passmatch) {
+        return res.status(400).json({ message: 'Old password is incorrect!' });
+      }
+  
+      if (!validatePassword) {
+        return res.status(400).json({ message: 'New password is not valid!' });
+      }
+  
+      // Hash the new password and update it
+      const hashedPassword = await bcryptjs.hash(newPassword, 10);
+      const updatedEmployee = await signupModel.findOneAndUpdate(
+        { _id: id },
+        { $set: { password: hashedPassword } },
+        { new: true }
+      );
+  
+      if (updatedEmployee) {
+        return res.status(200).json({ updatedEmployee, message: 'Password updated successfully!' });
+      } else {
+        return res.status(404).json({ message: 'Employee not found!' });
+      }
+    } catch (error) {
+      console.error('Error updating password:', error);
+      return res.status(500).json({ message: 'Server error', error: error.message });
+    }
+  };
